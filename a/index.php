d404c1822fbeb479a8b568bc82c96178139f1e29
nushratjahanurbi <nushratjahanurbi920c@gmail.com>
2026-04-22 17:13:57 +0600

    task done

---
+++ a/index.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Public Home</title>
+</head>
+<body>
+ 
+<table border="2" width="600" align="center" cellspacing="0">
+    <tr height="50">
+        <td align="right">
+             <a href="home.php">Home</a> |
+            <a href="login.php">Login</a> |
+            <a href="registration.php">Registration</a>
+        </td>
+    </tr>
+
+   
+    <tr height="200">
+        <td align="center">
+            <h2>Welcome to X Company</h2>
+        </td>
+    </tr>
+    <tr height="50">
+        <td align="center">
+            Copyright © 2026
+        </td>
+    </tr>
+ 
+</table>
+ 
+</body>
+</html>
+ 
